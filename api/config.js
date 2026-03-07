4a0c61c9d6095d9da50d2708469edb513b7ffb47
successcompanies <manager@thesuccesscompanies.com>
2026-03-07 12:47:56 -0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
-export default function handler(request, response) {
-  response.setHeader('Access-Control-Allow-Origin', '*');
+module.exports = function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   
-  return response.status(200).json({
+  return res.status(200).json({
     supabaseUrl: process.env.SUPABASE_URL,
     supabaseKey: process.env.SUPABASE_ANON_KEY
   });
-}
+};
